46ca3a92ca433a4f5813ac0acf71c6e2c99dc060
janvaljan7 <i.abed7@gmail.com>
2023-08-20 14:15:11 +0330

    cofigured docker compose and checks for db

---
+++ app/app/tests.py
@@ -0,0 +1,21 @@
+"""
+Sample tests
+"""
+from django.test import SimpleTestCase
+
+from app import calc
+
+
+class CalcTests(SimpleTestCase):
+    """Test the calc method"""
+
+    def test_add_numbers(self):
+        res = calc.add(5, 6)
+
+        self.assertEqual(res, 11)
+
+    def test_subtract_numbers(self):
+        """test subtracting the numbers"""
+        res = calc.subtract(10, 15)
+
+        self.assertEqual(res, 5)
